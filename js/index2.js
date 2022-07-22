4fd1a73b3037e6b69541dcbe32cfda7a132f7f61
KIMJIHYEON <kwlgus0318@gmail.com>
2022-07-22 18:20:05 +0900

    3d

---
+++ js/index2.js
@@ -0,0 +1,8 @@
+const leftdoor = document.querySelector('.left_section')
+
+leftdoor.addEventListener('click', () => {
+  const rightdoor = document.querySelector('.right_section')
+  const about = document.querySelector('.about')
+
+
+})
