f8a0226000a942207326db2870f0b2bdf3d0bbca
KIMJIHYEON <kwlgus0318@gmail.com>
2022-07-22 23:18:19 +0900

    반복문 완성

--- js/index2.js
+++ js/index2.js
@@ -1,8 +1,42 @@
-const leftdoor = document.querySelector('.left_section')
-
-leftdoor.addEventListener('click', () => {
-  const rightdoor = document.querySelector('.right_section')
-  const about = document.querySelector('.about')
+const leftdoor = document.querySelector('.left_section');
+const enter = document.querySelector('.enter');
 
 
-})
+
+const rightdoor = document.querySelector('.right_section');
+const about = document.querySelector('.about');
+
+
+enter.addEventListener('click', () => {
+
+  const close = document.querySelector('.Close');
+
+
+  if( enter.value === 'Open'){
+  leftdoor.style.transform = 'rotateY(10deg)';
+  leftdoor.style.transformOrigin = '500px';
+  rightdoor.style.transform='rotateY(-5deg)';
+  about.style.display = 'block';
+
+  enter.value = 'Close';
+  enter.textContent = 'Close';
+
+}
+
+else{
+  leftdoor.style.transform = 'none';
+  leftdoor.style.transformOrigin = 'none';
+  rightdoor.style.transform='none';
+  about.style.display = 'none';
+
+  enter.textContent = 'Open';
+  enter.value = 'Open';
+
+
+  }
+
+});
+  
+
+
+
