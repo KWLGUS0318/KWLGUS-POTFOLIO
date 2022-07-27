9cca43ea42c66a972d0df5677f2c839b45b18754
KIMJIHYEON <kwlgus0318@gmail.com>
2022-07-27 18:12:08 +0900

    commit

---
+++ hera/js/popular.js
@@ -0,0 +1,27 @@
+const pop = document.querySelector('.popular')
+const newThings = document.querySelector('.new')
+const choice = document.querySelector('.choice')
+
+
+console.log(pop,newThings,choice)
+
+let widthValue = '100vw';
+let heightValue = '50vh';
+let height= 'vh'
+let width= 'vw'
+
+
+pop.style.width = widthValue 
+pop.style.height = heightValue
+pop.style.border = '1px solid black';
+newThings.style.width = widthValue
+newThings.style.height = heightValue 
+newThings.style.border = '1px solid black';
+
+choice.style.width = widthValue
+choice.style.height = heightValue 
+choice.style.border = '1px solid black';
+
+
+
+
