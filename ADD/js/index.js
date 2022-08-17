bb068c5dba2a09143e8635546ece3626d6a2d00e
KIMJIHYEON <kwlgus0318@gmail.com>
2022-08-17 15:08:17 +0900

    타이머 설정

---
+++ ADD/js/index.js
@@ -0,0 +1,35 @@
+const rightbtn = document.querySelector('.right')
+console.log(rightbtn)
+
+const fistimg = document.querySelector('.first-recommand')
+console.log(fistimg)
+
+rightbtn.addEventListener('click', (e) => {
+  if (rightbtn.value === 'right') {
+    fistimg.style.display = 'none'
+  } else {
+
+  }
+
+})
+
+const timer = document.querySelector('.time')
+console.log(timer)
+
+let time = 600;
+let min = '';
+let sec = '';
+
+let clock = setInterval(() => {
+  min = parseInt(time / 60);
+  sec = time % 60;
+  timer.textContent = min + '분' + sec + '초'
+  time--;
+
+  if(time<0){
+    clearInterval(clock);
+    timer.textContent = '시간초과';
+  }
+
+}, 1000);
+
