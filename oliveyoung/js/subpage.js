b4ff68694152351172494529d71e0890542c6db1
KIMJIHYEON <kwlgus0318@gmail.com>
2022-09-08 23:08:34 +0900

    new

---
+++ oliveyoung/js/subpage.js
@@ -0,0 +1,28 @@
+const root = document.getElementById("root");
+const windowWidth = window.innerWidth;
+
+let eventHandle = 0;
+window.addEventListener("wheel", (event) => {
+  if (event.deltaY > 0) {
+    eventHandle++;
+    window.scrollTo({
+      left: windowWidth * eventHandle,
+      behavior: "smooth"
+    });
+  } else {
+    eventHandle--;
+    window.scrollTo({
+      left: windowWidth * eventHandle,
+      behavior: "smooth"
+    });
+  }
+  if (eventHandle < 0) {
+    eventHandle = 0;
+  }
+  if (eventHandle > 5) {
+    eventHandle = 5;
+  }
+});
+
+
+
